15762aaa28a74be257262473494073bbf33783b1
Gaige Pierce-Raison <gaige@chatsubo.net>
2014-04-15 22:21:04 -0700

    Lots of changes to test event handler, added event queue, etc.
    - Added code to actually watch events and push them onto a queue.
    - Currently have event handler for testing that just reads off events every seconds.
    - Reworked the collectPaths() function, it was not working as desired before.
    
    Currently a goroutine is used to start the watcher on the set of paths, after this routine starts
    the event handler passed by the user is now given a queue from which it gets the events.

--- fsmonitor.go
+++ fsmonitor.go
@@ -3,15 +3,17 @@ package main
 
 import (
 	"code.google.com/p/go.exp/fsnotify"
+	"github.com/hishboy/gocommons/lang"
 
 	"fmt"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 
 
-type Handler func([]string, []string)//watcher *fsnotify.Watcher)
+type Handler func([]string, []string, *lang.Queue)
 
 func isMember(item string, arr []string) bool {
 	for i := 0; i < len(arr); i++ {
@@ -23,20 +25,22 @@ func isMember(item string, arr []string) bool {
 }
 
 func collectPaths(paths []string) []string {
-	newPaths := make([]string, 1, 2)
+	newPaths := make([]string, 1, 1)
 
-	for _, path := range paths {
-		err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+	for _, thisPath := range paths {
+		err := filepath.Walk(thisPath, func(path string, info os.FileInfo, err error) error {
 			if info.IsDir() {
-				newPaths = append(paths, path)
-			}
-			return err
+				newPaths = append(newPaths, path)
+		}
+			return nil
 		})
 
 		if err != nil {
-			fmt.Println("Error walking tree: ", err, " on file: ", path)
+			fmt.Println(err)
 		}
 	}
+
+	fmt.Println(newPaths)
 	return newPaths
 }
 
@@ -44,6 +48,8 @@ func MonitorFileSystem(paths []string, excludes []string, handleEvents Handler) 
 	var watchedCount int = 0
 	paths = collectPaths(paths)
 	excludes = collectPaths(excludes)
+
+	eventQueue := lang.NewQueue()
 
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
@@ -53,7 +59,7 @@ func MonitorFileSystem(paths []string, excludes []string, handleEvents Handler) 
 
 	// establish watches
 	for _, path := range paths {
-		if !isMember(path, excludes) {
+		if !(isMember(path, excludes)) {
 			err = watcher.Watch(path)
 			if err != nil {
 				// report error up?
@@ -62,17 +68,32 @@ func MonitorFileSystem(paths []string, excludes []string, handleEvents Handler) 
 			watchedCount++
 		}
 	}
+
 	fmt.Println("Directories watched: ", watchedCount)
 
-	// call event handler
-	handleEvents(paths, excludes)
+	go func() {
+		for {
+			select {
+			case ev := <-watcher.Event:
+				eventQueue.Push(ev)
+				//fmt.Println(ev)
+			case err := <-watcher.Error:
+				fmt.Println(err)
+			}
+		}
+	}()
 
+	// call event handler
+	handleEvents(paths, excludes, eventQueue)
 }
 
 func main() {
-	a := []string{"/home/gaige/go/src/github.com/gaigepr/fsmonitor/"}
-	b := []string{"/home/gaige/go/src/github.com/gaigepr/fsmonitor/.git/"}
-	MonitorFileSystem(a, b, func(c, d []string) {
-		fmt.Println("Into event handler!")
+	a := []string{"/home/gaige/Dropbox/school/"}
+	b := []string{"/home/gaige/Dropbox/school/2013-2014/cs_301/"}
+	MonitorFileSystem(a, b, func(c, d []string, eventQueue *lang.Queue) {
+		for {
+			time.Sleep(1 * time.Second)
+			fmt.Println(eventQueue.Poll())
+		}
 	})
 }
