877f22f472316b908dbcd66f1bb88579a2c2d2d9
Gaige Pierce-Raison <gaige@chatsubo.net>
2014-05-10 16:04:47 -0700

    Fixes issue #1. New directories are scanned and all subdirectories are watched.
    Everytime a directory is created, it is scanned for subdirectories. In cases like `mkdir -p`
    usage, this ensures that all subdirectories get watched.

--- eventHandler.go
+++ eventHandler.go
@@ -62,8 +62,17 @@ func EventHandler(eventChan chan Event, watcher *fsnotify.Watcher) {
 		event := <-eventChan
 		switch {
 		case event.EventType == fsnotify.FSN_CREATE:
+			watcher.Watch(event.FilePath)
 			if event.IsDir {
-				watcher.Watch(event.FilePath)
+				allPaths := CollectPaths([]string{event.FilePath})
+				//fmt.Println(allPaths[1:])
+				//time.Sleep(1 * time.Millisecond)
+				for _, path := range allPaths[1:] {
+					err := watcher.Watch(path)
+					if err != nil {
+						fmt.Println("Error: ", err, "  establishing watch on: ", path)
+					}
+				}
 			}
 			fmt.Printf("EVENT %s: %s \t%v\n", "CREATE", event.FilePath, event.EventTime)
 
