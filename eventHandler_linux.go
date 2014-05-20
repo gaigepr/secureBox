e2be158014c4a8afce5cf50661f966afec0d0757
Gaige Pierce-Raison <gaige@chatsubo.net>
2014-05-20 13:37:35 -0700

    Event handler now deals with move to/from events assuming they happen sequentially. Added constants for the event types for readability:     DIR_CREATE instead of inotify.IN_ISDIR + inotify.IN_CREATE.

--- eventHandler_linux.go
+++ eventHandler_linux.go
@@ -7,28 +7,64 @@ import (
 	"code.google.com/p/go.exp/inotify"
 )
 
+const (
+	DIR_CREATE     = inotify.IN_CREATE + inotify.IN_ISDIR
+	DIR_DELETE     = inotify.IN_DELETE + inotify.IN_ISDIR
+	DIR_MOVE_FROM  = inotify.IN_MOVED_FROM + inotify.IN_ISDIR
+	DIR_MOVE_TO    = inotify.IN_MOVED_TO + inotify.IN_ISDIR
+	FILE_CREATE    = inotify.IN_CREATE
+	FILE_MODIFY    = inotify.IN_CLOSE_WRITE
+	FILE_DELETE    = inotify.IN_DELETE
+	FILE_MOVE_FROM = inotify.IN_MOVED_FROM
+	FILE_MOVE_TO   = inotify.IN_MOVED_TO
+)
+
 func EventHandler(watcher *inotify.Watcher) {
+	var moveFromEvent *inotify.Event
+
 	for {
 		select {
 		case event := <-watcher.Event:
 			switch {
-			case event.Mask == (inotify.IN_CREATE + inotify.IN_ISDIR):
-				fmt.Printf("EVENT %s: %s \t%v\n", "DIR CREATE", event.Name, time.Now())
+			default:
+				fmt.Println(event, time.Now())
 
-			case event.Mask == inotify.IN_CREATE:
-				fmt.Printf("EVENT %s: %s \t%v\n", "CREATE", event.Name, time.Now())
+			case event.Mask == DIR_CREATE:
+				watcher.Watch(event.Name)
+				fmt.Println(event.String(), time.Now())
 
-			case event.Mask == inotify.IN_CLOSE_WRITE:
-				fmt.Printf("EVENT %s: %s \t%v\n", "CLOSE_WRITE", event.Name, time.Now())
+			case event.Mask == FILE_CREATE:
+				fmt.Println(event.String(), time.Now())
 
-			case event.Mask == (inotify.IN_DELETE + inotify.IN_ISDIR):
-				fmt.Printf("EVENT %s: %s \t%v\n", "DIR DELETE", event.Name, time.Now())
+			case event.Mask == FILE_MODIFY:
+				fmt.Println(event.String(), time.Now())
 
-			case event.Mask == inotify.IN_DELETE:
-				fmt.Printf("EVENT %s: %s \t%v\n", "DELETE", event.Name, time.Now())
+			case event.Mask == DIR_DELETE:
+				watcher.RemoveWatch(event.Name)
+				fmt.Println(event.String(), time.Now())
 
-			case event.Mask == inotify.IN_MOVE:
-				fmt.Printf("EVENT %s: %s \t%v\n", "MOVE", event.Name, time.Now())
+			case event.Mask == FILE_DELETE:
+				fmt.Println(event.String(), time.Now())
+
+			case event.Mask == DIR_MOVE_FROM:
+				moveFromEvent = event
+				watcher.RemoveWatch(event.Name)
+				fmt.Println(event.String(), time.Now())
+
+			case event.Mask == DIR_MOVE_TO:
+				if event.Cookie == moveFromEvent.Cookie {
+					watcher.Watch(event.Name)
+					fmt.Println("\t", event.String(), time.Now())
+				}
+
+			case event.Mask == FILE_MOVE_FROM:
+				moveFromEvent = event
+				fmt.Println(event.String(), time.Now())
+
+			case event.Mask == FILE_MOVE_TO:
+				if event.Cookie == moveFromEvent.Cookie {
+					fmt.Println("\t", event.String(), time.Now())
+				}
 
 			}
 
