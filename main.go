355d6edc3469c16af50a54420ab1a85c3054208c
Gaige Pierce-Raison <gaige@chatsubo.net>
2014-05-21 19:30:44 -0700

    Now loading directory to watch/ignore from a config file.
    Must call secureBox with a command line argument specifying the path to the json config file.
    TODO: Handle the case when the confif file does not exist. Perhaps leave a default somewhere on the system?

--- main.go
+++ main.go
@@ -1,29 +1,54 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
 )
 
+type Configuration struct {
+	Username     string
+	Password     string
+	WatchRoots   []string
+	ExcludeRoots []string
+}
+
 func main() {
-	if len(os.Args) > 1 && os.Args[1] == "test" {
-		TestCrypto()
-		return
+	// Command line arg(s)
+	if !(len(os.Args) > 1) {
+		panic("Not enough command line args: Need path to config file!")
 	}
 
-	paths := []string{"/home/gaige/testing/"}
-	excludes := []string{"/home/gaige/testing/no_watch/"}
+	// Load or create the config file
+	config := Configuration{}
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		// If the config file is not found we need to make it
+		fmt.Println("Could not find config file or something: ", err)
+		file, err = os.Create(os.Args[1]);
+		if err != nil {
+			fmt.Println("ERROR creating file: ", err)
+		}
+	}
 
-	// Create watch, add directories to it
-	watchedCount, watcher := SetupWatch(paths, excludes)
+	// Decode config file into Configuration struct
+	decoder := json.NewDecoder(file)
+	if err = decoder.Decode(&config); if err != nil {
+		fmt.Println("ERROR parsing json: ", err)
+	}
+	fmt.Println(config)
+
+	// Create watch and add directories to it
+	watchedCount, watcher := SetupWatch(config.WatchRoots, config.ExcludeRoots)
 	fmt.Println("Directories watched: ", watchedCount)
 
 	go EventHandler(watcher)
 
+	// Wait for SIGINT
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, os.Kill)
-
 	for {
 		select {
 		case s := <-c:
